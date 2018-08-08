16ae7f309de5cecc75458678b891f32669c9ab0b
Home <home@Homes-MacBook-Air.local>
2018-08-09 00:34:05 +0200

    Correcting false use of “rtrim”

--- src/IBAServiceProvider.php
+++ src/IBAServiceProvider.php
@@ -145,7 +145,7 @@ class IBAServiceProvider extends Provider
 	{
 		Response::macro('asset', function ($asset_name, $uri, $disk = "ibook")
 		{
-			if ( substr($asset_name, -3) == 'mp4' )
+			if ( substr($asset_name, -4) === '.mp4' )
 			{
 				$file = '/var/www/lostideaslab/storage/' . $disk . '/' . $uri;
 				$mime = 'video/mp4';
@@ -221,14 +221,14 @@ class IBAServiceProvider extends Provider
 
 
 			}
-			else if (substr($asset_name, -4) == 'html')
+			elseif (substr($asset_name, -5) === '.html')
 			{
-				$uri = rtrim($uri, '.html') . '/';
+				$uri = preg_replace('/.html$/', '', $uri)  . '/';
 				return redirect($uri);
 			}
-			else if (substr($asset_name, -2) == 'md')
+			elseif (substr($asset_name, -3) === '.md')
 			{
-				$uri = rtrim($uri, '.md') . '/';
+				$uri = preg_replace('/.md$/', '', $uri) . '/';
 				return redirect($uri);
 			}
 			else
