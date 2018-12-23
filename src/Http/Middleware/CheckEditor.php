eb3de4683888675659107237805cb8eed4ad1e37
Home <home@Homes-MacBook-Air.local>
2018-12-23 13:04:22 +0100

    - patron management system (initiation)

---
+++ src/Http/Middleware/CheckEditor.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace PirateLP\IBA\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class CheckEditor
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+	    if ( Auth::user()->id != 1 )
+	    {
+		    return redirect('home');
+	    }
+        return $next($request);
+    }
+}
