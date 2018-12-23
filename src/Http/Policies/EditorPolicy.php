f05a07f64795ea0125eda28ebd568b345faee9a2
Home <home@Homes-MacBook-Air.local>
2018-12-23 13:04:24 +0100

    - patron management system (initiation)

---
+++ src/Http/Policies/EditorPolicy.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace PirateLP\IBA\Policies;
+
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class EditorPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+    
+    public function manage_atelier(User $user)
+	{
+		return $user->id == 1;
+	}
+}
